99b0204472f53b7617210d7d125f2c06e02e437c
EslamTaha-Dev <esalmtahadev07@gmail.com>
2026-07-14 22:16:24 +0300

    Add Footer Section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,32 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
   return (
-    <div></div>
-  )
-}
+    <footer>
+      <div className="info">
+        <p>
+          {" "}
+          More ways to shop: <span>Find an Apple Store</span> or{" "}
+          <span>other retailer</span> near you. Or call 000800 040 1966.
+        </p>
 
-export default Footer
+        <img src="/logo.svg" alt="Apple" />
+      </div>
+      <hr />
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+        <ul>
+          {footerLinks.map(
+            ({ label, link }: { label: string; link: string }) => (
+              <li key={label}>
+                <a href={link}>{label}</a>
+              </li>
+            ),
+          )}
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
